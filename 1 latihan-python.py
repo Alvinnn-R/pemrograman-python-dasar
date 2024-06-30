f2324812390a01f2485b2dd8112d7648426e8c90
Alvin <alvinramasaputra03@gmail.com>
2024-06-30 22:48:18 +0700

    feat: add basic python exercises

---
+++ 1 latihan-python.py
@@ -0,0 +1,17 @@
+# Berkenalan dengan Python
+
+print("Hello World!")
+
+greting = 'Hello Alvin'
+print(greting)
+
+tambah = 2 + 2
+print(tambah)
+
+'''
+Python input dan output
+Ini adalah block coment
+'''
+name = input("Masukan nama anda: ")
+print("Nama anda adalah :", name)
+
