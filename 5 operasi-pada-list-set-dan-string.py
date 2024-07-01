3aa5c5c1aee3e659f641740a1a854faf7b916883
Alvin <alvinramasaputra03@gmail.com>
2024-07-01 22:39:54 +0700

    feat: add operation on list set and string in python

--- 5 operasi-pada-list-set-dan-string.py
+++ 5 operasi-pada-list-set-dan-string.py
@@ -30,7 +30,7 @@ genap = [2, 4, 4, 6, 6, 6, 8, 10, 10]
 print(genap.count(6))
 
 '''program akan menghitung banyaknya substring/huruf "a" dalam string.'''
-string = "Belajar Python di Dicoding sangat menyenangkan"
+string = "Belajar Python di Kak Alvin sangat menyenangkan"
 substring = "a"
 print(string.count(substring))
 
@@ -39,10 +39,10 @@ print(string.count(substring))
 Anda bisa menggunakan operator ini untuk memastikan suatu nilai ada dalam list bahkan dalam string. 
 Operator in dan not in akan mengembalikan nilai boolean True atau False.'''
 
-kalimat = "Belajar Python di Dicoding sangat menyenangkan"
-print('Dicoding' in kalimat)
+kalimat = "Belajar Python di Kak Alvin sangat menyenangkan"
+print('Alvin' in kalimat)
 print('tidak' in kalimat)
-print('Dicoding' not in kalimat)
+print('Alvin' not in kalimat)
 print('tidak' not in kalimat)
 
 print('\n')
@@ -87,7 +87,7 @@ print(kendaraan)
 
 '''Pada kode di atas, mengurutkan variabel "kendaraan" secara descending atau menurun. 
 Hal ini membuktikan bahwa secara default fungsi sort() akan mengurutkan secara ascending atau menaik.'''
-# urutan = ['Dicoding', 1, 2, 'Indonesia', 3]
+# urutan = ['Alvin', 1, 2, 'Indonesia', 3]
 # urutan.sort()
 
 # print(urutan)
@@ -108,3 +108,42 @@ Jadi, sort() akan menganggap semua objek menggunakan huruf kecil, tanpa mengubah
 kendaraan.sort(key=str.lower)
 
 print(kendaraan)
+
+
+"""
+TODO:
+Diberikan sebuah variabel berisi nilai list dengan nama "var_list". 
+Berdasarkan list tersebut, cari hal-hal berikut ini:
+- Panjang list tersebut dan simpan dengan nama variabel "panjang".
+- Nilai maksimal list tersebut dan simpan dengan nama variabel "maksimal".
+- Nilai minimal list tersebut dan simpan dengan nama variabel "minimal".
+- Berapa banyak angka 605132 dalam list tersebut dan simpan dalam variabel bernama "banyak"
+
+Tips:
+- Anda bisa menggunakan berbagai kode yang ada di materi operasi, 
+  tidak diperbolehkan memasukan nilai secara langsung.
+"""
+
+# Jangan ubah kode ini
+var_list = [792564, 465231, 203748, 981037, 857219, 314092, 608345, 123907, 736890, 985026, 
+179430, 450218, 680934, 543187, 978260, 283045, 617902, 405826, 820913, 731095, 
+592403, 109237, 874956, 605132, 214978, 674519, 391047, 825160, 509317, 768490, 
+950283, 307491, 487610, 532198, 605132, 160984, 609873, 245016, 879043, 734126, 
+351809, 670594, 920873, 605132, 596410, 135890, 804512, 683420, 290753, 931560, 
+175430, 950672, 378490, 284105, 746098, 503624, 605132, 167432, 974810, 519463, 
+407835, 740326, 281709, 630921, 953284, 605132, 429731, 183607, 369012, 541380, 
+605132, 217605, 496803, 827309, 153607, 605132, 720459, 381904, 594031, 810235, 
+673925, 492157, 835096, 260481, 956024, 540329, 806295, 320158, 751903, 980465, 
+235780, 857943, 605132, 125094, 620493, 913250
+]
+
+# TODO: Buat kode Anda di bawah ini
+panjang = len(var_list)
+maksimal = max(var_list)
+minimal = min(var_list)
+banyak = var_list.count(605132)
+
+print(panjang)
+print(maksimal)
+print(minimal)
+print(banyak)
